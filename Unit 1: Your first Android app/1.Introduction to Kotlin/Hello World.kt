5ebb8d9e3861367e787d62366ba4d39bfcc0bbb5
Violet's Foxes <111033412+KoukeNeko@users.noreply.github.com>
2022-12-04 11:48:26 +0800

    create greeting card project

--- Unit 1: Your first Android app/1.Introduction to Kotlin/Hello World.kt
+++ Unit 1: Your first Android app/1.Introduction to Kotlin/Hello World.kt
@@ -1,5 +1,13 @@
 package org.kotlinlang.play         // 1
+/**
+ * This program displays the number of messages
+ * in the user's inbox.
+ */
 
 fun main() {                        // 2
     println("Hello, World!")        // 3
+    val count: Int = 2
+    val count2 = 2
+    println(count)
+    println(count2)
 }
