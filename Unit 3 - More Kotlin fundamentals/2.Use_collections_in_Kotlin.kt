05ea2fee1802c487bf5ab39d27230e86f49706a7
Violet's Foxes <111033412+KoukeNeko@users.noreply.github.com>
2022-12-13 23:07:24 +0800

    2.Use_collections_in_Kotlin

--- Unit 3 - More Kotlin fundamentals/2.Use_collections_in_Kotlin.kt
+++ Unit 3 - More Kotlin fundamentals/2.Use_collections_in_Kotlin.kt
@@ -15,12 +15,61 @@ fun main() {
     // val newSolarSystem = arrayOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune", "Pluto")
     // println(newSolarSystem[8])
 
-    val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
-    println(solarSystem.size)
-    println(solarSystem[2])
-    println(solarSystem.get(3))
+    // val solarSystem = listOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
 
-    println(solarSystem.indexOf("Earth"))
-    println(solarSystem.indexOf("Pluto"))
+    // val solarSystem = mutableListOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
 
+    // println(solarSystem.size)
+    // println(solarSystem[2])
+    // println(solarSystem.get(3))
+
+    // println(solarSystem.indexOf("Earth"))
+    // println(solarSystem.indexOf("Pluto"))
+
+    // solarSystem.add("Pluto")
+    // solarSystem.add(3, "Theia")
+
+    // solarSystem[3] = "Future Moon"
+    // // solarSystem.remove("Future Moon")
+    
+
+    // println(solarSystem[3])
+    // println(solarSystem[9]) 
+    // solarSystem.removeAt(9)
+
+    // println(solarSystem.contains("Pluto"))
+    // println("Future Moon" in solarSystem)
+
+    // val solarSystem = mutableSetOf("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+    // println(solarSystem.size)
+    // solarSystem.add("Pluto")
+    // println(solarSystem.size)
+    // println(solarSystem.contains("Pluto"))
+    // solarSystem.add("Pluto")
+    // println(solarSystem.size)
+    // solarSystem.remove("Pluto")
+    // println(solarSystem.size)
+    // println(solarSystem.contains("Pluto"))
+
+    val solarSystem = mutableMapOf<String, Int>(
+    "Mercury" to 0,
+    "Venus" to 0,
+    "Earth" to 1,
+    "Mars" to 2,
+    "Jupiter" to 79,
+    "Saturn" to 82,
+    "Uranus" to 27,
+    "Neptune" to 14
+    )
+
+
+println(solarSystem.size)
+solarSystem["Pluto"] = 5
+println(solarSystem.size)
+println(solarSystem["Pluto"])
+println(solarSystem["Theia"])
+solarSystem.remove("Pluto")
+println(solarSystem.size)
+solarSystem["Jupiter"] = 78
+println(solarSystem["Jupiter"])
 }
